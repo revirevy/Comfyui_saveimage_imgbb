a0400d601a54f1ef4c797a9e059a50d8a377a195
revirevy <revirevy@gmail.Com>
2024-09-18 14:18:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
+"""
+@author: N.RHEVI
+@title: save image to imgbb
+@nickname: save image to imgbb
+@description: This custom node allow save image to imgbb.
+"""
+
 from .SimpleMath import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
