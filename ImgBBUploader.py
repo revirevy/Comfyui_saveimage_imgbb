dedadcf0ce2447e314fe4b770a2eb8d8d0222040
revirevy <revirevy@gmail.Com>
2024-09-24 22:31:20 +0200

    Update ImgBBUploader.py

--- ImgBBUploader.py
+++ ImgBBUploader.py
@@ -7,61 +7,50 @@ from PIL.PngImagePlugin import PngInfo
 import io, time
 from torchvision import transforms
 
+import requests
+import json
+import torch
+from comfy.model_management import get_torch_device
 
-# class SaveImage:
-#     def __init__(self):
-#         self.output_dir = folder_paths.get_output_directory()
-#         self.type = "output"
-#         self.prefix_append = ""
-#         self.compress_level = 4
+class LLM_prompt_generator:
+    def __init__(self):
+        self.type = "output"
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "LLM_API": ("STRING", {"default": ""}),
+                "user_prompt": ("STRING", {"default": ""}),
+                "model": ("STRING", {"default": ""}),
+            },
+        }
 
-#     @classmethod
-#     def INPUT_TYPES(s):
-#         return {
-#             "required": {
-#                 "images": ("IMAGE", {"tooltip": "The images to save."}),
-#                 "filename_prefix": ("STRING", {"default": "ComfyUI", "tooltip": "The prefix for the file to save. This may include formatting information such as %date:yyyy-MM-dd% or %Empty Latent Image.width% to include values from nodes."})
-#             },
-#             "hidden": {
-#                 "prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"
-#             },
-#         }
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "make_api_request"
+    CATEGORY = "LLM"
 
-#     RETURN_TYPES = ()
-#     FUNCTION = "save_images"
-
-#     OUTPUT_NODE = True
-
-#     CATEGORY = "image"
-#     DESCRIPTION = "Saves the input images to your ComfyUI output directory."
-
-#     def save_images(self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None):
-#         filename_prefix += self.prefix_append
-#         full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
-#         results = list()
-#         for (batch_number, image) in enumerate(images):
-#             i = 255. * image.cpu().numpy()
-#             img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
-#             metadata = None
-#             if not args.disable_metadata:
-#                 metadata = PngInfo()
-#                 if prompt is not None:
-#                     metadata.add_text("prompt", json.dumps(prompt))
-#                 if extra_pnginfo is not None:
-#                     for x in extra_pnginfo:
-#                         metadata.add_text(x, json.dumps(extra_pnginfo[x]))
-
-#             filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
-#             file = f"{filename_with_batch_num}_{counter:05}_.png"
-#             img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
-#             results.append({
-#                 "filename": file,
-#                 "subfolder": subfolder,
-#                 "type": self.type
-#             })
-#             counter += 1
-
-#         return { "ui": { "images": results } }
+    def make_api_request(self, LLM_API, user_prompt,model_name):
+        try:
+            response = requests.post(
+                url="https://openrouter.ai/api/v1/chat/completions",
+                headers={
+                    "Authorization": f"Bearer {LLM_API}",
+                },
+                data=json.dumps({
+                    "model": model_name,
+                    "messages": [
+                        {"role": "user", "content": user_prompt}
+                    ]
+                })
+            )
+            
+            if 'choices' in response.json():
+                return (response.json().get('choices')[0].get('message').get('content'),)
+            else:
+                return ("Error: No response from API",)
+        except Exception as e:
+            return (f"Error: {str(e)}",)
 
 
 
