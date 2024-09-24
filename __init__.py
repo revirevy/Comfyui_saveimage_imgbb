6215f6b3fbfd60466eaf1de19618f5c0a2ba6b16
revirevy <revirevy@gmail.Com>
2024-09-24 22:51:09 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,6 +5,6 @@
 @description: This custom node allow save image to imgbb.
 """
 
-from .ImgBBUploader import ImgBBUploader, LLM_prompt_generator, NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS, 
+from .ImgBBUploader import ImgBBUploader, LLM_prompt_generator, NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['ImgBBUploader', 'LLM_prompt_generator', 'NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
